6a3d96b2a6f7d6561672fc7394c0e620fff442ac
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-22 22:14:01 +0400

    Палиндром

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа, которая принимает на вход число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Write number: ");
+String num = Console.ReadLine();
+bool isPalindrom = true;
+
+for (int i = 0; i < num.Length / 2; i++)
+{
+    if (num[i] != num[num.Length - 1 - i])
+    { 
+        isPalindrom = false;
+        break;
+    }
+}
+if (isPalindrom)
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else 
+{
+    Console.WriteLine("Число не является палиндромом");
+}
