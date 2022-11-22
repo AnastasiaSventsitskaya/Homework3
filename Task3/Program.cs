6aef68070112a3011a21eaea995750445b209d65
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-23 02:11:03 +0400

    Кубы чисел

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает таблицу кубов чисел от 1 до N
+
+
+
+Console.WriteLine("Write number");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (var counter = 1; counter <= N; counter++) 
+{
+  Console.Write(Math.Pow(counter, 3) + " ");
+}
