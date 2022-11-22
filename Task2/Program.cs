17e866943dec9dd90b17a6b6a7d209b81a8fa893
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-23 02:02:16 +0400

    точки в 3D

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход кардинаты двух точек и находит расстояние между ними в 3D
+
+
+Console.WriteLine("write X1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("write Y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("write Z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("write X2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("write Y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("write Z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1,2) + Math.Pow(z2 - z1,2) );
+
+Console.WriteLine(result);
+
